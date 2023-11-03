da908f6a53cdca37406a4c4be839eb503a27c10a
tcampsusu <susuame1batch@gmail.com>
2023-11-03 20:55:12 +0900

    商品情報編集機能-02

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new,:create]
-  before_action :set_item, only: [:edit,:show ]
+  before_action :authenticate_user!, only: [:new,:create,:edit]
+  before_action :set_item, only: [:edit,:show ,:update ]
   before_action :move_to_index, only: [:edit]
   
 
@@ -27,7 +27,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
    if @item.update(item_params)
     redirect_to item_path
   else
